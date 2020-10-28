bbaf64268cc3f582eaa3fad8553068f5d4639b49
Dr-Fabulous <71410010+Dr-Fabulous@users.noreply.github.com>
2020-10-28 22:18:13 +0000

    added simple documentation to random

--- mm/include/mm/random.h
+++ mm/include/mm/random.h
@@ -2,6 +2,11 @@
 #define MM_RANDOM_H
 #include "mm/common.h"
 
+/*! \file */
+
+/*!
+	\brief Simple xorshift random number generator.
+*/
 struct mm_random {
 	unsigned long counter;
 	unsigned long a;
@@ -10,7 +15,14 @@ struct mm_random {
 	unsigned long d;
 } mm_random_t;
 
+/*!
+	\brief Get a new random number within the passed range.
+*/
 MM_API unsigned long mm_random_next( struct mm_random *this, unsigned long min, unsigned long max );
+
+/*!
+	\breif Reset & reseed a mm_random.
+*/
 MM_API void mm_random_reset( struct mm_random *this, unsigned long seed );
 
 #endif
