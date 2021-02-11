b7499c14258e2b8d5b0240a45508183592a9b46d
Dr-Fabulous <7140010+Dr-Fabulous@users.noreply.github.com>
2021-02-11 19:55:30 +0000

    fixed posix feature test added uninterrupted mm_full_sleep ( will resume sleep after interrupts )

--- mm/include/mm/sleep.h
+++ mm/include/mm/sleep.h
@@ -3,28 +3,37 @@
 #include "mm/common.h"
 
 #if __STDC_VERSION__ >= 201112L && !defined( __STDC_NO_THREADS__ )
-#include <thread.h>
+#include <threads.h>
 #define mm_sleep thrd_sleep
-#elif MM_USING_POSIX
-#define _POSIX_C_SOURCE 199309L
+#elif MM_USING_POSIX && _POSIX_C_SOURCE >= 199309L
 #include <time.h>
 #define mm_sleep nanosleep
-#undef _POSIX_C_SOURCE
 #elif MM_USING_WINDOWS
 #include <syncapi.h>
 #include "mm/time.h"
 
-static inline int mm_sleep( struct timespec *dur, struct timespec *rem ) {
+static inline int mm_sleep( struct timespec const * const dur, struct timespec * const rem ) {
 	Sleep( ( DWORD ) ( dur->tv_sec * 1000 + ( dur->tv_nsec / 1000000 ) ) );
 
 	if ( rem ) {
-		rem->tv_sec = 0;
-		rem->tv_nsec = 0;
+		rem->tv_sec = ( time_t ) 0;
+		rem->tv_nsec = 0L;
 	}
 
 	return 0;
 }
 
 #else
-#error "Unsupported platform"
+#error "Sleep unsupported on current platform"
+#endif
+
+static inline void mm_full_sleep( struct timespec const * const duration ) {
+	struct timespec dur = *duration;
+	struct timespec rem;
+
+	while( mm_sleep( &dur, &rem ) ) {
+		dur = rem;
+	}
+}
+
 #endif
